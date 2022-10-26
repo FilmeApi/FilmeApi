ab8a59c3fb9e4eca181820cb8d4138fe926b4efd
Leonardo Castilho <leonardo.castilho@viannasempre.com.br>
2022-10-26 10:59:07 -0300

    Retornando Informações da API
    Adicionado método para retornar a lista de filmes, adicionado campo ID e adicionada uma enumeração de ids momentâneamente

--- FilmesAPI/FilmesAPI/Models/Filme.cs
+++ FilmesAPI/FilmesAPI/Models/Filme.cs
@@ -4,6 +4,8 @@ namespace FilmesAPI.Models
 {
     public class Filme
     {
+        public int Id { get; set; }
+
         [Required(ErrorMessage = "O campo título é obrigatório")]
         public string Titulo { get; set; }
 
@@ -15,5 +17,6 @@ namespace FilmesAPI.Models
 
         [Range(1, 600, ErrorMessage = "A duração deve ter no mínimo 1 e no máximo 600 minutos")]
         public int Duracao { get; set; }
+
     }
 }
